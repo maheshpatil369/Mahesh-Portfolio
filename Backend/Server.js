707dda5f79a73c931f09079245f95da0f52773ca
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-06-16 16:18:33 +0530

    Update server

--- Backend/Server.js
+++ Backend/Server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use('/api', SendEmail);
 
 app.listen(5000, () => {
-  console.log('✅ Server is running on port 5000');
+  console.log('✅ Server is running successfully on port 5000');
 });
